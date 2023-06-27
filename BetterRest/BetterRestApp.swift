124b9a62f0ca2e036916ee8c948c5c5e0b921558
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-06-26 19:46:41 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Isaac Dyess on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
